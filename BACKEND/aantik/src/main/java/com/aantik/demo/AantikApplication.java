fe616993b68a52daaef09962bd9e3208bcdb5157
tuchys <tuchysguer@hotmail.com>
2022-09-21 15:01:04 -0500

    actualizado

--- BACKEND/aantik/src/main/java/com/aantik/demo/AantikApplication.java
+++ BACKEND/aantik/src/main/java/com/aantik/demo/AantikApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.bezkoder.spring.files.upload.service.FilesStorageService;
+import com.aantik.demo.service.FilesStorageService;
 
 
 
--- FRONTEND/src/views/UploadFiles.vue
+++ FRONTEND/src/views/UploadFiles.vue
@@ -1,47 +1,75 @@
 <template>
-  <div>
-    <div v-if="currentFile" class="progress">
-      <div
-        class="progress-bar progress-bar-info progress-bar-striped"
-        role="progressbar"
-        :aria-valuenow="progress"
-        aria-valuemin="0"
-        aria-valuemax="100"
-        :style="{ width: progress + '%' }"
-      >
-        {{ progress }}%
-      </div>
-    </div>
+    <b-container>  
+       <div>
+          <sidebar-menu-akahon 
+            @search-input-emit="search"
+          />
+        </div> 
+    <b-row>
+      <b-col>
+        <div class="hello" >
+          <h3>Gestor de carga</h3>
+          <br>
+          <p align=left>
+          Debe seleccionar el archivo .csv que cumpla con los parametros del sistema.
+          
+           <br>
+          <router-link to="/ayuda">Revisar parametros</router-link>
+          <br></p>
+        </div>
+        <div>
+          <div v-if="currentFile" class="progress">
+            <div
+              class="progress-bar progress-bar-info progress-bar-striped"
+              role="progressbar"
+              :aria-valuenow="progress"
+              aria-valuemin="0"
+              aria-valuemax="100"
+              :style="{ width: progress + '%' }"
+            >
+              {{ progress }}%
+            </div>
+          </div>
 
-    <label class="btn btn-default">
-      <input type="file" ref="file" @change="selectFile" />
-    </label>
+          <label class="btn btn-default">
+            <input type="file" ref="file" @change="selectFile" />
+          </label>
 
-    <button class="btn btn-success" :disabled="!selectedFiles" @click="upload">
-      Upload
-    </button>
+          <button class="btn btn-success" :disabled="!selectedFiles" @click="upload">
+            Upload
+          </button>
 
-    <div class="alert alert-light" role="alert">{{ message }}</div>
+          <div class="alert alert-light" role="alert">{{ message }}</div>
 
-    <div class="card">
-      <div class="card-header">List of Files</div>
-      <ul class="list-group list-group-flush">
-        <li
-          class="list-group-item"
-          v-for="(file, index) in fileInfos"
-          :key="index"
-        >
-          <a :href="file.url">{{ file.name }}</a>
-        </li>
-      </ul>
-    </div>
-  </div>
+          <div class="card">
+            <div class="card-header">List of Files</div>
+            <ul class="list-group list-group-flush">
+              <li
+                class="list-group-item"
+                v-for="(file, index) in fileInfos"
+                :key="index"
+              >
+                <a :href="file.url">{{ file.name }}</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </b-col>
+      <b-col>
+      
+      </b-col>
+    </b-row>
+  </b-container>  
 </template>
 
 <script>
+import SidebarMenuAkahon from "@/components/SideBar.vue"
 import UploadService from "@/service/UploadFilesService";
 
 export default {
+  components: {
+    SidebarMenuAkahon,
+  },
   name: "upload-files",
   data() {
     return {
