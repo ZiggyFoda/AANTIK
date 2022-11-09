52bfede42bf7bbf70db0cdfda3a8ba12b9a8a96b
Puma <duarte-juan@javeriana.edu.co>
2022-11-09 13:44:27 -0500

    Update indicadoresEdit3.vue

--- FRONTEND/src/views/admin/indicadoresEdit3.vue
+++ FRONTEND/src/views/admin/indicadoresEdit3.vue
@@ -1,198 +1,128 @@
 <template>
-  <b-container>  
-       <div>
-          <sidebar-menu-akahon 
-            @search-input-emit="search"
-          />
-        </div> 
-    <b-row>
-      <b-col>
-        <div class="hello">
-          <h3>Gestor de estudiantes</h3>
-        </div>
-
-
-        Listado de estudiantes preinscritos
-        <br>
-        <div>
-
-          
-         <b-editable-table striped hoverw
+  <div>
+    <b-editable-table striped hoverw
       bordered
       class="editable-table"
       v-model="users"
       :fields="fields"
-      @input-change="handleInput">
-
-          </b-editable-table>
-        </div>
-        Listado de estudiantes inscritos
-
-           <br>
-           <div>
-             <b-table striped hover id="pages-table" :items="users" :fields="fields">
-               <template #cell(Editar)="row">
-                <router-link to="/stuEdit">
-                 <b-button class="mr-2">Editar
-                 </b-button></router-link>
-               </template>
-               <template #cell(Eliminar)="row">
-                 <b-button class="mr-2">Eliminar
-                 </b-button>
-               </template>
-             </b-table>
-           </div>
-           <br>  
-              </b-col>
-              <b-col>
-              
-              </b-col>
-            </b-row>
-          </b-container>   
-        </template>
+      @input-change="handleInput"
+    >
+      <template #cell(isActive)="data">
+        <span v-if="data.value">Yes</span>
+        <span v-else>No</span>
+      </template>
+      <template #cell(Editar)="row">
+              <router-link to="/preEdit">
+              <b-button class="mr-2">Editar
+              </b-button></router-link>
+            </template>
+    </b-editable-table>
+    <pre>
+      {{ users }}
+    </pre>
+  </div>
+</template>
 
 <script>
+import BEditableTable from "bootstrap-vue-editable-table";
 import axios from "axios";
-import SidebarMenuAkahon from "@/components/SideBar.vue"
-import service from "@/service/studentData"
 export default {
   components: {
-    SidebarMenuAkahon,
+    BEditableTable,
   },
-name: "user",
-
-data() {
-
-  return{
-    users: {
+  data() {
+    return {
+      users: {
 
 id: null,
 
 nombre: null,
 
-asignatura: null,
+Delete: null,
 
-identificacion: null,
 
 } ,
+      fields: [
+        {
+          key: "id",
+          label: "Id",
+          type: "text",
+          editable: true,
+          placeholder: "Enter ID",
+          class: "id",
+        },
+        {
+          key: "nombre",
+          label: "Nombre",
+          type: "text",
+          editable: true,
+          placeholder: "Enter name...",
+          class: "nombre",
+        },
+        {
+          key: "Delete",
+          label: "Delete",
+          type: "text",
+          editable: true,
+          placeholder: "Enter name...",
+          class: "nombre",
+        },
+      ],
+    };
+  },
+  mounted() {
 
-fields: [
-
-  { key: "id" },
-
-  { key: "nombre" },
-
-  { key: "asignatura" },
-
-  { key: "Editar" },
-
-  { key: "Eliminar" }
-
-],
-
-
-
-  }
-},
-mounted() {
-
-  axios.get("http://localhost:8080/api/gesStu").then(
+axios.get("http://localhost:8080/api/gesStu").then(
 
 (response) => {
 
-  this.users= response.data;
+this.users= response.data;
 
 }
 
 );
-}
-
-
-
-
-}
-
-
-
-/*
-export default {
-  name: 'estudiantesCord',
-  props: {
-    msg: String
-  },
-   components: {
-    SidebarMenuAkahon,
-  },
+},
   methods: {
-    search() {
-      
-    },
-    update(data) {
-      // I need to disable the button here
-      this.output = data;
-      data.item.name = "Dave";
-      this.$refs["btn" + data.index].disabled = true      
-    }
+    handleInput(value, data) {},
   },
-  
-      return {
-        items: [
-          { ID: 40, Nombres: 'Dickerson', Apellido: 'Macdonald', Asignatura: '2353001', Clase: '4202', Emprendimiento: 'emprendimiento 2' },
-          { ID: 21, Nombres: 'Larsen', Apellido: 'Shaw', Asignatura: '2353001', Clase: '4202' },
-          { ID: 89, Nombres: 'Geneva', Apellido: 'Wilson', Asignatura: '2353001', Clase: '4206', Emprendimiento: 'emprendimiento 2' },
-          { ID: 38, Nombres: 'Jami', Apellido: 'Carney', Asignatura: '2353001', Clase: '4208', Emprendimiento: 'emprendimiento 1',isActive: true }
-        ],
-        items2: [
-          { ID: 40, Nombres: 'Dickerson', Apellido: 'Macdonald', Asignatura: '2353001', Requisitos: 'Satisfecho' },
-          { ID: 21, Nombres: 'Larsen', Apellido: 'Shaw', Asignatura: '2353001', Requisitos: 'Pendiente' },
-          { ID: 89, Nombres: 'Geneva', Apellido: 'Wilson', Asignatura: '2353001', Requisitos: 'Pendiente' },
-          { ID: 38, Nombres: 'Jami', Apellido: 'Carney', Asignatura: '2353001', Requisitos: 'Satisfecho' }
-        ],fields: [
-          {
-            key: "ID",
-            label: "ID",
-            sortable: true
-          },
-          { key: "Nombres" },
-          { key: "Apellido" },
-          { key: "Asignatura" },
-          { key: "Clase" },
-          { key: "Emprendimiento" },
-          { key: "Editar" },
-          { key: "Eliminar" }
-        ],fields2: [
-          {
-            key: "ID",
-            label: "ID",
-            sortable: true
-          },
-          { key: "Nombres" },
-          { key: "Apellido" },
-          { key: "Asignatura" },
-          { key: "Requisitos" },
-          { key: "Editar" },
-          { key: "Eliminar" }
-        ]
-      }      
-  }
-}*/
-
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+table.editable-table {
+  margin: auto;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+table.editable-table td {
+  vertical-align: middle;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.editable-table .data-cell {
+  padding: 8px;
+  vertical-align: middle;
 }
-a {
-  color: #42b983;
+
+.editable-table .custom-checkbox {
+  width: 50px;
+}
+
+.name-col {
+  width: 120px;
+}
+
+.department-col {
+  width: 150px;
+}
+
+.age-col {
+  width: 100px;
+}
+
+.date-col {
+  width: 200px;
+}
+
+.is-active-col {
+  width: 100px;
 }
 </style>
