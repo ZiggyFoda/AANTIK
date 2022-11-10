34d7c6beb22e3d5156e01b8d58b9dde5a8c5b15e
tuchys <tuchysguer@hotmail.com>
2022-11-10 15:43:24 -0500

    Datos basicos lectura

--- BACKEND/aantik/src/main/java/com/aantik/demo/model/ModDocente.java
+++ BACKEND/aantik/src/main/java/com/aantik/demo/model/ModDocente.java
@@ -1,5 +1,25 @@
 package com.aantik.demo.model;
 
 public class ModDocente {
-
+	public int id;
+	public String nombre;
+	public String vinculacion;
+	public String titulo;
+	public String especialidad;
+	public String asignatura;
+	public String telefono;
+	public String correo;
+	public String oficina;
+	public String direcRes;
+	public String localidad;
+	public String sectoEc;
+	public String HisAse;
+	public String contacto;//medio de comunicacion que prefiere**
+	public String lineaAc;//**p
+	public String secEco2;//**
+	public String tipoOS;
+	public String horarioNotif;
+	public String horarioAtencion;
+	public String NOdisponibilidad;
+	public boolean limitacion;
 }
