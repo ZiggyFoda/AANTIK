1846b44cc0448d02ef1e6ec7abb22dc7ae7d8289
tuchys <tuchysguer@hotmail.com>
2022-11-13 08:15:27 -0500

    Carga ORg Social completo

--- BACKEND/aantik/src/main/java/com/aantik/demo/control/orgSocController.java
+++ BACKEND/aantik/src/main/java/com/aantik/demo/control/orgSocController.java
@@ -3,15 +3,13 @@ package com.aantik.demo.control;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.aantik.demo.cargaUsuarios.leerEmprendimientos;
-import com.aantik.demo.model.ModEmprendimiento;
+import com.aantik.demo.cargaUsuarios.leerEntidades;
+import com.aantik.demo.model.ModOrgSocial;
 import com.aantik.demo.service.OrgSocialCRUD;
 
 @Controller
@@ -20,27 +18,27 @@ public class orgSocController {
 	OrgSocialCRUD orgScService;
 	
 	@GetMapping("/orgSCargaM")
-	public ResponseEntity<ModEmprendimiento[]> cargaStu() {
-		ModEmprendimiento est[] = new ModEmprendimiento[2];
+	public ResponseEntity<ModOrgSocial[]> cargaStu() {
+		ModOrgSocial est[] = new ModOrgSocial[2];
 		try {
-			ModEmprendimiento [] empLista = new ModEmprendimiento[500];
-			leerEmprendimientos excelEmp = new leerEmprendimientos();
+			ModOrgSocial [] orgLista = new ModOrgSocial[50];
+			leerEntidades excelOrg = new leerEntidades();
 			FileInputStream fis2;
 			try {
-				fis2 = new FileInputStream(new File("Datos basicos-formato.xlsx"));
-				excelEmp.getEmprendimientos(fis2,empLista);
+				fis2 = new FileInputStream(new File("Datos basicos-formato.xlsx"));			
+				excelOrg.getEntidades(fis2,orgLista);
 				
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			try {
-				orgScService.saveAll(empLista);
+				orgScService.saveAll(orgLista);
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-				return new ResponseEntity<ModEmprendimiento[]>(est, HttpStatus.OK);
+				return new ResponseEntity<ModOrgSocial[]>(est, HttpStatus.OK);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			//e.printStackTrace();
